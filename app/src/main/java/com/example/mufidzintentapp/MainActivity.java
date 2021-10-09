3923f7dde17e5a3599f255c346ffc7c0d919ed45
Mufidz <mufidz.xtav2.05@gmail.com>
2021-10-09 15:03:43 +0700

    practice2

---
+++ app/src/main/java/com/example/mufidzintentapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.mufidzintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btnMoveActivity = findViewById(R.id.btn_pindah_activity);
+        btnMoveActivity.setOnClickListener(this);
+
+        Button btnMoveDataActivity = findViewById(R.id.btn_pindah_with_data);
+        btnMoveDataActivity.setOnClickListener(this);
+
+        Button btnMoveObjectActivity = findViewById(R.id.btn_pindah_with_object);
+        btnMoveObjectActivity.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btn_pindah_activity){
+            Intent moveIntent = new Intent(MainActivity.this, MoveActivity.class);
+            startActivity(moveIntent);
+        } else if(v.getId() == R.id.btn_pindah_with_data){
+            Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
+            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Mufidz");
+            moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 10);
+            startActivity(moveWithDataIntent);
+        } else if (v.getId() == R.id.btn_pindah_with_object){
+            Person person = new Person();
+            person.setName("Mufidz");
+            person.setAge(22);
+            person.setEmail("mufidz@amikompurwokerto.ac.id");
+            person.setCity("Purwokerto");
+            Intent moveWithObjectIntent = new Intent(MainActivity.this, MoveWithObjectActivity.class);
+            moveWithObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person);
+            startActivity(moveWithObjectIntent);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mufidzintentapp/MoveWithDataActivity.java
@@ -0,0 +1,26 @@
+package com.example.mufidzintentapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MoveWithDataActivity extends AppCompatActivity {
+
+    public static final String EXTRA_AGE = "extra_age";
+    public static final String EXTRA_NAME = "extra_name";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_move_with_data);
+
+        TextView tcDataReceived = findViewById(R.id.tv_data_received);
+
+        String name = getIntent().getStringExtra(EXTRA_NAME);
+        int age = getIntent().getIntExtra(EXTRA_AGE, 0);
+
+        String text = "Nama : " + name + ", Your Age : " + age ;
+        tcDataReceived.setText(text);
+    }
+}
